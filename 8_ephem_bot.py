025b238876acf0f0442bdc9874c0f812a8e136e9
Valeria <valery.y.s@gmail.com>
2020-05-29 23:10:04 +0300

    bot update

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -16,6 +16,7 @@ import logging
 import ephem
 import settings as st
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -31,6 +32,16 @@ PROXY = {
     }
 }
 
+PLANETS = {
+    'Mercury': ephem.Mercury,
+    'Venus': ephem.Venus,
+    'Mars': ephem.Mars,
+    'Jupiter': ephem.Jupiter,
+    'Saturn': ephem.Saturn,
+    'Uranus': ephem.Uranus,
+    'Neptune': ephem.Neptune
+}
+
 
 def greet_user(update, context):
     text = 'Вызван /start'
@@ -38,22 +49,40 @@ def greet_user(update, context):
     update.message.reply_text('Добрый день!')
 
 
-def talk_to_me(update, context):
-    user_text = getattr(getattr(update, 'message'), 'text')
-    if user_text.lower() == '/planet mars':
-        m = ephem.Mars()
-        m.compute()
-        update.message.reply_text('Mars is in ' + ephem.constellation(m)[1])
-    elif user_text.lower() == '/planet venus':
-        v = ephem.Venus()
-        v.compute()
-        update.message.reply_text('Venus is in ' + ephem.constellation(v)[1])
-    elif user_text.lower() == '/planet jupiter':
-        j = ephem.Jupiter()
-        j.compute()
-        update.message.reply_text('Jupiter is in ' + ephem.constellation(j)[1])
+def next_full_moon(update, context):
+    today = datetime.today()
+    result = ephem.next_full_moon(today)
+    print(result)
+    update.message.reply_text(f'Дата следующего полнолуния: {result}')
+
+
+def word_form(wordcount):
+    if 11 <= wordcount % 100 <= 14:
+        return 'слов'
+    elif wordcount % 10 == 1:
+        return 'слово'
+    elif 2 <= wordcount % 10 <= 4:
+        return 'слова'
     else:
-        update.message.reply_text(user_text)
+        return 'слов'
+
+
+def talk_to_me(update, context):
+    user_command = update.message.text.split()[0]
+    user_text = update.message.text.split()[1:]
+    if user_command == '/planet':
+        user_planet = update.message.text.split()[1]
+        p = PLANETS.get(user_planet)()
+        p.compute()
+        result = ephem.constellation(p)[1]
+        update.message.reply_text(f'{user_planet} is in ' + result)
+    elif user_command == '/wordcount':
+        if user_text:
+            wordcount = len([word for word in user_text if word.isalpha()])
+            words = word_form(wordcount)
+            update.message.reply_text(f'{wordcount} {words}')
+    else:
+        update.message.reply_text(update.message.text)
 
 
 def main():
@@ -61,6 +90,7 @@ def main():
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("next_full_moon", next_full_moon))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
