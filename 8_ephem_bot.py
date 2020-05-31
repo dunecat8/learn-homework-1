cda8c6ce57aa35aaba1feeea8ec0c02adef51b79
Valeria <valery.y.s@gmail.com>
2020-05-31 23:34:46 +0300

    bot update: cities

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -15,7 +15,7 @@
 import logging
 import ephem
 import settings as st
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from datetime import datetime
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -42,6 +42,9 @@ PLANETS = {
     'Neptune': ephem.Neptune
 }
 
+CITIES = [
+    'Москва', 'Архангельск', 'Калининград', 'Дубна', 'Санкт-Петербург', 'Мурманск', 'Красноярск', 'Дублин', 'Нью-Йорк', 'Амстердам'
+]
 
 def greet_user(update, context):
     text = 'Вызван /start'
@@ -67,6 +70,45 @@ def word_form(wordcount):
         return 'слов'
 
 
+def cities_start(update, context):
+    context.user_data["cities"] = CITIES
+    context.user_data["round"] = 1
+    update.message.reply_text('Назовите город')
+    return 'cities_main'
+
+
+def cities_main(update, context):
+    user_city = update.message.text
+    print(user_city, context.user_data["round"], context.user_data["cities"], sep = '\n' )
+    if context.user_data["round"] != 1:
+        last_char = context.user_data['last_char']
+        if not user_city.startswith(last_char):
+            update.message.reply_text(f'Город должен быть на букву {last_char}!')
+            return 'cities_main'
+    if not user_city in context.user_data["cities"]:
+        update.message.reply_text('Такого города нет в списке! Назовите другой')
+        return 'cities_main'
+    context.user_data["cities"].remove(user_city)
+    last_char = user_city[-1].upper()
+    print(last_char)
+    try:
+        bot_city = [city for city in context.user_data["cities"] if city.startswith(last_char)][0]
+        update.message.reply_text(bot_city)
+        update.message.reply_text('Ваш ход!')
+        context.user_data["cities"].remove(bot_city)
+        context.user_data['last_char'] = bot_city[-1].upper()
+        context.user_data["round"] += 1
+        return 'cities_main'
+    except IndexError:
+        update.message.reply_text('Я сдаюсь!')
+        return ConversationHandler.END
+
+
+def cancel(update, context):
+    update.message.reply_text('Спасибо за игру!')
+    return ConversationHandler.END
+
+
 def talk_to_me(update, context):
     user_command = update.message.text.split()[0]
     user_text = update.message.text.split()[1:]
@@ -75,7 +117,7 @@ def talk_to_me(update, context):
         p = PLANETS.get(user_planet)()
         p.compute()
         result = ephem.constellation(p)[1]
-        update.message.reply_text(f'{user_planet} is in ' + result)
+        update.message.reply_text(f'{user_planet} is in {result}' )
     elif user_command == '/wordcount':
         if user_text:
             wordcount = len([word for word in user_text if word.isalpha()])
@@ -90,8 +132,12 @@ def main():
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(ConversationHandler(entry_points=[CommandHandler("cities", cities_start)],
+                                       states={'cities_main': [MessageHandler(Filters.text & (~Filters.command), cities_main)]},
+                                       fallbacks=[CommandHandler('cancel', cancel)]))
     dp.add_handler(CommandHandler("next_full_moon", next_full_moon))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
 
     mybot.start_polling()
     mybot.idle()
@@ -99,3 +145,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#pass_args=True, pass_user_data=True
